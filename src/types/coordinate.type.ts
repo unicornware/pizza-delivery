85bf63e08d3a35a51a01bc949be5bd20013a53e2
Lexus Drumgold <unicornware@flexdevelopment.llc>
2022-03-15 18:27:02 -0400

    feat(types): `Coordinate`

---
+++ src/types/coordinate.type.ts
@@ -0,0 +1,12 @@
+/**
+ * @file Type Definitions - Coordinate
+ * @module pizza-delivery/types/Coordinate
+ */
+
+/**
+ * One member in a pair of numbers that represent a single point on a
+ * two-dimensional grid.
+ */
+type Coordinate = bigint | number
+
+export default Coordinate
---
+++ src/types/index.ts
@@ -0,0 +1,6 @@
+/**
+ * @file Entry Point - Type Definitions
+ * @module pizza-delivery/types
+ */
+
+export type { default as Coordinate } from './coordinate.type'
